a6e32bf80c40e5c85168f56de6507031c40b2d5e
Chris McClimans <chris@hippiehacker.org>
2011-09-09 06:01:38 +1200

    Bios boot screen image and databag update...

--- infrastructure/freshvirtualbox.rb
+++ infrastructure/freshvirtualbox.rb
@@ -1,5 +1,7 @@
 require 'virtualbox'
 require 'fileutils'
+require 'chef'
+Chef::Config.from_file '.chef/knife.rb'
 
 boxname="randomname#{rand.to_s[2..5+1]}"
 vbox = VirtualBox::VM.create boxname
@@ -9,6 +11,27 @@ vbox.description="A Box to Remember"
 vbox.memory_size = 360
 vbox.audio_adapter.enabled=false
 vbox.usb_controller.enabled=false
+
+newhd=VirtualBox::HardDrive.new
+newhd.location=File.join(File.dirname(vbox.settings_file_path),vbox.name+'.vdi') #within the VM dir
+gigabyte=1000*1000*1024
+newhd.logical_size=10*gigabyte
+newhd.save
+
+controller_name='Ye Olde IDE Controller'
+vbox.with_open_session do |session|
+  machine = session.machine
+  #possibly change the screen on each boot... for demo?
+  session.machine.bios_settings.logo_image_path='/var/www/ii.bmp' #256/8bit BMP
+  session.machine.bios_settings.logo_display_time=3000 #3 seconds in ms
+  session.machine.bios_settings.pxe_debug_enabled=true
+  machine.add_storage_controller controller_name, :ide
+  machine.attach_device(controller_name, 0, 0, :hard_disk, newhd.interface)
+  machine.attach_device(controller_name, 0, 1, :dvd, nil) 
+end
+
+
+vbox.storage_controllers[0].controller_type = :ich6 #or :piix4
 
 # this will boot from network only if we can't boot from disk
 vbox.boot_order=[:hard_disk ,:network,:null,:null]
@@ -20,29 +43,19 @@ nic.attachment_type = :nat
 nic.enabled = true
 nic.save
 
-newhd=VirtualBox::HardDrive.new
-newhd.location=File.join(File.dirname(vbox.settings_file_path),vbox.name+'.vdi') #within the VM dir
-gigabyte=1000*1000*1024
-newhd.logical_size=10*gigabyte
-newhd.save
-
-controller_name='Ye Olde IDE Controller'
-vbox.with_open_session do |session|
-  machine = session.machine
-  machine.add_storage_controller controller_name, :ide
-  machine.attach_device(controller_name, 0, 0, :hard_disk, newhd.interface)
-  machine.attach_device(controller_name, 0, 1, :dvd, nil) 
-end
-
-vbox.storage_controllers[0].controller_type = :ich6 #or :piix4
 vbox.save
 
-#"#{nic.mac_address}","ComputerName","#{vbox.name}"
-#"#{vbox.name}","top_scripts","vboxbase.bat"
+if not Chef::DataBag.cdb_list.include? 'virtualboxen'
+  Chef::DataBag.json_create({'name'=>'virtualboxen'}).save
+end
 
-# So we need to update some data on chef-server
-# and trigger a chef-client run on the pxe_boot server
-# what is the best way... databag? node-data?
+Chef::DataBagItem.json_create({
+    'data_bag'=>'virtualboxen',
+    'raw_data'=> {
+      'id'=>vbox.name,
+      'mac_address'=>nic.mac_address
+    }
+  }).save
 
 vbox.start 
 
